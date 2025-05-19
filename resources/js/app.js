9fe975292a1e849c5d0a7f2ae0b0cd6025828648
risam926 <risamagic926@gmail.com>
2025-05-20 08:41:48 +0900

    step8-6

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,16 +2,6 @@ import './bootstrap';
 
 console.log("hello");
 
-const buttons = document.querySelectorAll('.btn-danger');
-buttons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        if (confirm('本当に削除しますか？')) {
-            alert('削除しました。');
-        } else {
-            alert('削除をキャンセルしました。');
-        }
-    });
-}); 
 
 $(document).ready(function(){
     // 共通データ取得関数
